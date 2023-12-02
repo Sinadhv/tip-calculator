5f64a40d05c750faeff54986e5986e74a1a995d6
sina dehvari <cnadhv7@gmail.com>
2023-12-02 21:22:14 +0330

    update script.js , update style.css

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculateTip(){
+
+
+    //get the input type values
+    const billAmount = parseFloat(document.getElementById("bill-amount").value);
+    const tipPercentage = parseFloat(document.getElementById("tip-percentage").value);
+
+    // calculate the tip and total amount
+
+    const tipAmount = billAmount * (tipPercentage/100);
+    const totalAmount = tipAmount +billAmount;
+
+    // display the results
+    document.getElementById("tip-amount").textContent = '$'+ tipAmount.toFixed(2);
+    document.getElementById("total-amount").textContent = '$'+ totalAmount.toFixed(2);
+
+}
